ecb4a1afd84dc2d711a465bb191d69b16d33b159
beatrizJ-P <beatrice.matthias@mymona.uwi.edu>
2025-11-07 17:39:01 -0500

    completed javascript for newsletter subscription form

--- scripts.js
+++ scripts.js
@@ -1,1 +1,28 @@
 /* Add your JavaScript to this file */
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    const submitBtn = document.querySelector('button');
+    const messageDiv = document.getElementsByClassName('message')[0];
+
+    submitBtn.addEventListener('click', function(e) {
+        e.preventDefault();
+        const emailInput = document.querySelector('input[name="email"]');
+        const emailAddress = emailInput.value;
+        messageDiv.classList.remove('error', 'success');
+        messageDiv.innerHTML = '';
+        if(emailAddress === '') {
+            messageDiv.innerHTML = 'Please enter a valid email address.';
+            messageDiv.classList.add('error');
+            return;
+        }
+        else
+        {
+            messageDiv.innerHTML = 'Thank you! Your email address ' + emailAddress + ' has been added to our mailing list.';
+            messageDiv.classList.add('success');
+            emailInput.value = '';
+            return
+        }
+    });
+
+});
